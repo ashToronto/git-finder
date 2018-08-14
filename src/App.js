c32aaf955b4336683fa47b06629783a75089951b
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-14 22:24:11 +0000

    basic layout form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user: {}
+    }
+  }
+
+  getUser = () => {
+    const name = this.ref.name.value
+    console.log(name)
+  }
+
   render() {
+    const { user } = this.state.user;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type='text' placeholder='Enter a username' ref='name' />
+        <button onClick={ this.getUser }></button>
       </div>
     );
   }
